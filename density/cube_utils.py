6eca27c3badb7e0242ca49562d73ccbf917d83b2
pkluber <gkluber@sbcglobal.net>
2025-03-01 09:12:33 +0800

    add support for cheap LDA method

--- density/cube_utils.py
+++ density/cube_utils.py
@@ -124,7 +124,16 @@ def write_single_cube(filename, resolution=0.1, write_cube=True, charge=0):
         cube.write(rho, '{}.cube'.format(basename),
             comment='Cube_vector {}'.format(box))
 
-    
+
+def rks_lda(m: gto.Mole) -> np.ndarray | None:
+    mf = dft.RKS(m)
+    mf.grids.level = 1
+    mf.xc = 'lda'
+    mf.kernel()
+    if not mf.converged:
+        return None
+    return mf.make_rdm1(ao_repr=True)
+
 def dimer_cube_difference(filename, method, resolution=0.1, extension=5.0, charges=None, write_cube=False, path=None):
     '''
     Reads in a dimer XYZ and prints out a .cube file containing
@@ -142,8 +151,8 @@ def dimer_cube_difference(filename, method, resolution=0.1, extension=5.0, charg
     if path == None:
         path = os.getcwd()
 
-    if method not in ['HF', 'MP2', 'PBE0']:
-        raise ValueError('Methods currently implemented: HF, MP2, PBE0 only.')
+    if method not in ['HF', 'MP2', 'PBE0', 'LDA']:
+        raise ValueError('Methods currently implemented: HF, MP2, PBE0, LDA only.')
     # Get density matrices
     if method == 'HF':
         dim_mf = scf.RHF(dimer)
@@ -192,6 +201,15 @@ def dimer_cube_difference(filename, method, resolution=0.1, extension=5.0, charg
         m2_mf.xc = 'pbe0'
         m2_mf.kernel()
         mono2_dm = m2_mf.make_rdm1(ao_repr=True)
+    elif method == 'LDA':
+        dimer_dm = rks_lda(dimer)
+        mono1_dm = rks_lda(mono1)
+        mono2_dm = rks_lda(mono2)
+
+        # Skip this if the DFT failed to converge
+        if dimer_dm is None or mono1_dm is None or mono2_dm is None:
+            print(f'{filename} failed to converge!', flush=True)
+            return
 
     # Generate the cube dimensions based on the shape of the dimer
     orig = generate_uniform_grid(dimer, spacing=resolution, rotate=False, verbose=False)[1]
