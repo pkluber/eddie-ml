d4a17a6bd6d6e82f70b46b0358320b8876c9ad58
pkluber <gkluber@sbcglobal.net>
2025-03-17 15:46:18 +0800

    add test file

---
+++ inspect_hdf5.py
@@ -0,0 +1,29 @@
+import h5py
+
+# Open the HDF5 file
+with h5py.File('output.hdf5', 'r') as f:
+    # List all groups and datasets in the file
+    print("Keys in file: %s" % list(f.keys()))
+    
+    X = f['system'][:]
+
+with h5py.File('output_pca.hdf5', 'r') as fd:
+    Vs = fd['value'][:]
+    print(list(fd.keys()))
+    print(Vs.shape)
+
+# Parse energies.dat
+energies = {}
+with open('energies.dat') as fd:
+    lines = fd.readlines()
+    for line in lines:
+        xyz, ie = line.split(' ')
+        system_name = xyz[:-4]
+        ie = float(ie)
+    
+        energies[system_name] = ie
+
+print(list(energies.items())[0])
+
+test_str = X[0].decode('ascii')
+print(test_str, energies[test_str])
