a391d7de2004a1b9f1a0141f725ce4f1fe92b526
pkluber <gkluber@sbcglobal.net>
2025-08-19 17:30:14 +1000

    bug fixes until i realized dual-basis not supported by pyscf

--- get_energies.py
+++ get_energies.py
@@ -5,7 +5,12 @@ from density.cube_utils import xyz_to_Mol, dimerxyz_to_Mol
 
 from pyscf import scf, mp
 from pyscf.gto import Mole
-from pyscf.lib import config
+
+try:
+    import gpu4pyscf
+    use_gpu = True
+except ImportError:
+    use_gpu = False
 
 NEUTRAL_TAR = 'data/neutral-dimers.tar.gz' 
 
@@ -21,20 +26,22 @@ def list_contents(tarfile_path: str) -> list[str]:
 
 NEUTRAL_SYSTEMS = list_contents(NEUTRAL_TAR)
 
-if config.use_gpu:
+if use_gpu:
     print('GPU is enabled!')
 else:
     print('GPU is not enabled :C')
 
 def gpuify(x):
-    if config.use_gpu:
+    if use_gpu:
         return x.to_gpu()
     return x
 
 def mp2(mol: Mole) -> mp.MP2:
     mf = gpuify(scf.RHF(mol))
+    mf.basis = 'aug-cc-pvqz'
     mf.kernel()
     mp2 = gpuify(mp.MP2(mf))
+    mp2.basis = 'cc-pvtz'
     mp2.kernel()
     return mp2
     
@@ -46,16 +53,20 @@ def srs_mp2_int_energy(dimer: Mole, mono1: Mole, mono2: Mole):
     # Ensure basis is cc-pVTZ for SRS-MP2
     dimer.basis = mono1.basis = mono2.basis = 'cc-pvtz' 
     
-    d_mp2 = mp2(dimer), m1_mp2 = mp2(mono1), m2_mp2 = mp2(mono2)
+    d_mp2 = mp2(dimer) 
+    m1_mp2 = mp2(mono1)
+    m2_mp2 = mp2(mono2)
     
     e_os_int = d_mp2.e_corr_os - m1_mp2.e_corr_os - m2_mp2.e_corr_os
     e_ss_int = d_mp2.e_corr_ss - m1_mp2.e_corr_ss - m2_mp2.e_corr_ss
     eps_ds = e_os_int / e_ss_int
 
     if eps_ds >= 1:
-        c_os = 1.640, c_ss = 0
+        c_os = 1.640
+        c_ss = 0
     else:
-        c_os = 0.660, c_ss = 1.140
+        c_os = 0.660
+        c_ss = 1.140
 
     uncorr_int = d_mp2._scf.e_tot - m1_mp2._scf.e_tot - m2_mp2._scf.e_tot
     
