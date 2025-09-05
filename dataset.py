a92217610bb1e0f3ba6d1ea17845686a97a27ac0
pkluber <gkluber@sbcglobal.net>
2025-09-05 14:45:53 +0800

    add charge to dataset

--- dataset.py
+++ dataset.py
@@ -20,6 +20,7 @@ class UEDDIEDataset(Dataset):
             X = f['value'][:]
             S = [system.decode('ascii') for system in f['system'][:]]
             E = [element.decode('ascii') for element in f['species'][:]]
+            C = [int(round(charge) for charge in f['charge'][:]]
 
         self.systems = []
 
@@ -39,20 +40,22 @@ class UEDDIEDataset(Dataset):
             x = X[xi]
             s = S[xi]
             e = E[xi]
-            elem = (e, x)
+            c = C[xi]
+            elem = (e, c, x)
             if s not in self.atoms:
                 self.atoms[s] = [elem]
             else:
                 self.atoms[s].append(elem)
         
-        X_list = [[x for _, x in self.atoms[s]] for s in self.systems]
-        E_list = [[e for e, _ in self.atoms[s]] for s in self.systems]
+        X_list = [[x for _, _, x in self.atoms[s]] for s in self.systems]
+        E_list = [[e for e, _, _ in self.atoms[s]] for s in self.systems]
+        C_list = [[c for _, c, _ in self.atoms[s]] for s in self.systems]
         
         # Convert E_list to numerical form
         conversion_key = {'H': 0, 'C': 1, 'N': 2, 'O': 3}
         E_list = [[conversion_key[e] for e in e_list] for e_list in E_list]
 
-        # Pad X_list and E_list 
+        # Pad X_list, C_list, and E_list  
         max_size = 0
         for x in X_list:
             if len(x) > max_size:
@@ -60,10 +63,12 @@ class UEDDIEDataset(Dataset):
 
         X_list = pad(X_list, max_size, [0.0] * len(X_list[0][0]))
         E_list = pad(E_list, max_size, -1)
+        C_list = pad(C_list, max_size, 0)
 
         # Finally convert to torch tensors
         self.X = torch.tensor(np.array(X_list))
         self.E = torch.tensor(np.array(E_list))
+        self.C = torch.tensor(np.array(C_list))
 
         # Don't forget about energies
         self.Y = torch.tensor(np.array(list(self.energies.values())))
@@ -74,9 +79,9 @@ class UEDDIEDataset(Dataset):
 
     def get(self, index, return_name: bool = False):
         if return_name:
-            return self.X[index, ...], self.E[index, ...], self.Y[index, ...], self.systems[index]
+            return self.X[index, ...], self.E[index, ...], self.C[index, ...], self.Y[index, ...], self.systems[index]
 
-        return self.X[index, ...], self.E[index, ...], self.Y[index, ...]
+        return self.X[index, ...], self.E[index, ...], self.C[index, ...], self.Y[index, ...]
     
     def __len__(self):
         return self.X.shape[0]
@@ -90,9 +95,17 @@ def get_dataloader(batch_size: int = 16, shuffle: bool = True):
 
 if __name__ == '__main__':
     dataloader = get_dataloader()
-    x_sample, e_sample, y_sample = next(iter(dataloader))
-    print(f'x shape: {x_sample.shape}\ne shape: {e_sample.shape}\ny shape: {y_sample.shape}')
-    print(f'x dtype: {x_sample.dtype}\ne dtype: {e_sample.dtype}\ny dtype: {y_sample.dtype}')
+    x_sample, e_sample, c_sample, y_sample = next(iter(dataloader))
+    
+    print(f'x shape: {x_sample.shape}')
+    print(f'e shape: {e_sample.shape}')
+    print(f'c shape: {c_sample.shape}')
+    print(f'y shape: {y_sample.shape}')
+
+    print(f'x dtype: {x_sample.dtype}')
+    print(f'e dtype: {e_sample.dtype}')
+    print(f'c dtype: {c_sample.dtype}')
+    print(f'y dtype: {y_sample.dtype}')
 
 
 
