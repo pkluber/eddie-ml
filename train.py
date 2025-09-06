2eb63c34a869241d3490543280540609a61000f1
pkluber <gkluber@sbcglobal.net>
2025-09-06 08:44:53 +0800

    update train.py

--- train.py
+++ train.py
@@ -13,10 +13,10 @@ torch.set_default_dtype(torch.float64)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Load dataloader
-dataloader = dataset.get_dataloader(batch_size=1)
+dataloader = dataset.get_dataloader(batch_size=4)
 
 # Initialize model
-x_sample, _, _ = next(iter(dataloader))
+x_sample, _, _, _ = next(iter(dataloader))
 model = UEDDIENetwork(x_sample.shape)
 model.to(device)
 
@@ -29,11 +29,11 @@ n_epoch = 2000
 losses = []
 for epoch in range(n_epoch):
     total_loss = 0
-    for X, E, Y in dataloader:
-        X, E, Y = X.to(device), E.to(device), Y.to(device)
+    for X, E, C, Y in dataloader:
+        X, E, C, Y = X.to(device), E.to(device), C.to(device), Y.to(device)
         optimizer.zero_grad()
         
-        Y_pred = model(X, E)
+        Y_pred = model(X, E, C)
         loss = loss_function(Y_pred, Y) 
         loss.backward()
         optimizer.step()
@@ -43,7 +43,9 @@ for epoch in range(n_epoch):
     losses.append(total_loss)
 
     if epoch % 5 == 0:
-        print(f'Epoch {epoch}, loss: {loss.item()}', flush=True)
+        print(f'Epoch {epoch}, loss: {losses[-1]}', flush=True)
 
-torch.save(model, 'model.pt')
-np.save('losses.npy', np.array(losses))
+    if epoch % 20 == 0:
+        torch.save(model, 'model.pt')
+        np.save('losses.npy', np.array(losses))
+
